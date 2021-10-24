1e930f369b2dabc7ead8b9fd871a55f614f7e493
Danyelss <mos.dani@yahoo.com>
2021-10-24 12:26:06 +0300

    Advanced to page 70, database in progress

---
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -0,0 +1,26 @@
+package tacos.data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import tacos.Ingredient;
+
+@Repository
+public class JdbcIngredientRepository implements IngredientRepository{
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Iterable<Ingredient> findAll() {
+        return jdbcTemplate.query(
+                "select id, name, type from Ingredient," +
+                        this::mapRowToIngredient);
+    }
+}
